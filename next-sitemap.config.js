5b30fcd1a6d64a98007133df9a69ed7bc60e483f
khalid Reda <khalidaziz2198@gmail.com>
2024-11-23 20:39:39 +0200

    Upload the sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: "https://namratech.com",
+  generateRobotsTxt: true, // Generates a robots.txt file
+  sitemapSize: 5000, // Optional: Limit the number of URLs per sitemap
+};
